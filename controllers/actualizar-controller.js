19e08d21c3c6427297bdb909554f489867dcfa4b
Oskar Lopez <puntoexactogeek@gmail.com>
2022-12-17 17:49:44 -0500

    clase05 completa

--- controllers/actualizar-controller.js
+++ controllers/actualizar-controller.js
@@ -2,7 +2,8 @@ import { clientServices } from "../services/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacion = () => {
+//*Funcion async - await (mdn-Español)
+const obtenerInformacion = async () => {
   const url = new URL(window.location);
   const id = url.searchParams.get("id");
 
@@ -13,11 +14,20 @@ const obtenerInformacion = () => {
   const nombre = document.querySelector("[data-nombre]");
   const email = document.querySelector("[data-email]");
 
-  clientServices.detalleCliente(id).then((perfil) => {
-    nombre.value = perfil.nombre;
-    email.value = perfil.email;
-  });
+  //*try...catch (mdn - español)
+  try {
+    const perfil = await clientServices.detalleCliente(id);
+    if (perfil.nombre && perfil.email) {
+      nombre.value = perfil.nombre;
+      email.value = perfil.email;
+    } else {
+      throw new error();
+    }
+  } catch (error) {
+    window.location.href = "/screens/error.html";
+  }
 };
+
 obtenerInformacion();
 
 formulario.addEventListener("submit", (evento) => {
